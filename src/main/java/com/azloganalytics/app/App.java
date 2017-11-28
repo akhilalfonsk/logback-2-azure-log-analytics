41f333361c1f63ea92251afe379a4dacd600aafc
akhilalfonsk <akhilalfonsk@users.noreply.github.com>
2017-11-28 18:06:23 +0530

    Commits

--- src/main/java/com/azloganalytics/app/App.java
+++ src/main/java/com/azloganalytics/app/App.java
@@ -3,14 +3,34 @@ package com.azloganalytics.app;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.codearte.jfairy.Fairy;
+import io.codearte.jfairy.producer.company.Company;
+import io.codearte.jfairy.producer.person.Person;
+import io.codearte.jfairy.producer.person.PersonProperties;
+
 public class App {
 
 		final static Logger logger = LoggerFactory.getLogger(App.class);
 		
 		public static void main(String[] args) {
-			logger.info("Msg #1");
-			logger.warn("Msg #2");
-			logger.error("Msg #3");
-			logger.debug("Msg #4");
+			
+			Fairy fairy = Fairy.create();
+			for (int i = 0; i < 10; i++) {
+				Company company = fairy.company();
+				Person person = fairy.person(PersonProperties.withCompany(company));
+				logger.warn("User First name:" + person.getFirstName());
+				logger.info("User Last name:" + person.getLastName());
+				if(person.isMale()) {
+					logger.debug("User is Male");	
+				}else {
+					logger.debug("User is Female");	
+				}
+				
+				logger.debug("User City:" + person.getAddress().getCity());
+				logger.debug("User Company:" + person.getCompany().getEmail());
+				logger.debug("User Company Email:" + person.getCompanyEmail());
+
+			}
+
 		}
 }
